f903b7e74e47fb95144190085394a5c6acf56253
cagdasmer <43043172+cagdasmer@users.noreply.github.com>
2018-09-13 14:54:52 +0300

    Update import-h2.sql (NOT IMPORTED)
    This is file is here only for the purpose of providing the sql queries being used, without the Java syntax.

--- Todos-Server/src/main/resources/import-h2.sql
+++ Todos-Server/src/main/resources/import-h2.sql
@@ -1,15 +1,12 @@
 DROP TABLE IF EXISTS todo_list CASCADE;
 CREATE TABLE todo_list(id SERIAL, 
-					   name VARCHAR(255), 
-					   PRIMARY KEY (id));
+					   name VARCHAR(255));
         
 DROP TABLE IF EXISTS todo_item;
-CREATE TABLE todo_item(id BIGINT, 
+CREATE TABLE todo_item(id SERIAL, 
 					   name VARCHAR(255), 
 					   todo_list_id BIGINT, 
-					   completed BOOLEAN, 
-					   CONSTRAINT items_ibfk_1 FOREIGN KEY (todo_list_id) REFERENCES todo_list (id),
-					   CONSTRAINT test_pkey PRIMARY KEY (id, todo_list_id));
+					   completed BOOLEAN);
         
 
 INSERT INTO todo_list(name) VALUES('Research');
@@ -17,10 +14,10 @@ INSERT INTO todo_list(name) VALUES('Study');
 INSERT INTO todo_list(name) VALUES('Develop');
 INSERT INTO todo_list(name) VALUES('Document');
         
-INSERT INTO todo_item(id, name, todo_list_id, completed) VALUES(1, 'Look for online tutorials', 1, true);
-INSERT INTO todo_item(id, name, todo_list_id, completed) VALUES(2, 'Look for books', 1, true);
-INSERT INTO todo_item(id, name, todo_list_id, completed) VALUES(1, 'Study the documents', 2, false);
-INSERT INTO todo_item(id, name, todo_list_id, completed) VALUES(1, 'Develop the back-end', 3, false);
-INSERT INTO todo_item(id, name, todo_list_id, completed) VALUES(2, 'Develop the front-end', 3, false);
-INSERT INTO todo_item(id, name, todo_list_id, completed) VALUES(1, 'Prepare documentation for the application', 4, false);
+INSERT INTO todo_item(name, todo_list_id, completed) VALUES('Look for online tutorials', 1, true);
+INSERT INTO todo_item(name, todo_list_id, completed) VALUES('Look for books', 1, true);
+INSERT INTO todo_item(name, todo_list_id, completed) VALUES('Study the documents', 2, false);
+INSERT INTO todo_item(name, todo_list_id, completed) VALUES('Develop the back-end', 3, true);
+INSERT INTO todo_item(name, todo_list_id, completed) VALUES('Develop the front-end', 3, false);
+INSERT INTO todo_item(name, todo_list_id, completed) VALUES('Prepare documentation for the application', 4, false);
 														
